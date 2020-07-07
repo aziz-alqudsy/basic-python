c0f73712dc002af2ce394dc37ea7d18a5f982839
aziz <aziz@ruangguru.com>
2020-07-08 04:27:57 +0700

    input and update readme

---
+++ input.py
@@ -0,0 +1,14 @@
+# input string
+my_name = "Aziz"
+your_name = input("enter your name here : ")
+print(f"hello {your_name}, my name is {my_name}")
+
+
+# input integer
+age = input("how old are you : ")
+age_to_int = int(age)
+print(f"you are lived for {age_to_int * 12} months")
+
+age = int(input("how old are you : "))
+count = age * 12
+print(f"you are lived for {count} months")
